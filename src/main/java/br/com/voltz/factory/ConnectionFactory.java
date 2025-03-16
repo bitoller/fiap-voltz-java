1754107cbf3ad25e3d2cb6073da4bc45f1be63c6
lucashcribeiro <lucashcribeiro@gmail.com>
2025-03-16 17:13:11 -0300

    timeout bd

--- src/main/java/br/com/voltz/factory/ConnectionFactory.java
+++ src/main/java/br/com/voltz/factory/ConnectionFactory.java
@@ -3,6 +3,8 @@ package br.com.voltz.factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ConnectionFactory {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
@@ -10,6 +12,8 @@ public class ConnectionFactory {
     private static final String PASSWORD = "060892";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        connection.setNetworkTimeout(Executors.newSingleThreadExecutor(), (int) TimeUnit.MINUTES.toMillis(2));
+        return connection;
     }
 }
